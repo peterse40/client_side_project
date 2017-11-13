b2cf91dcb9094af010d8b3b18fc508e8d8a4d93a
peterse40 <peterse40@github.com>
2017-11-13 11:45:55 -0600

    added base project assets

---
+++ src/data/cars.js
@@ -0,0 +1,35 @@
+// src/data/carvana.js
+
+const carvana = [
+	{
+
+		'id': '082017',
+		'make': 'Ford',
+		'model': 'Focus',
+		'year': '2013',
+		'color': 'black',
+		'img': 'ford-focus.jpg',
+	
+	},
+
+	{
+        'id': 'SvT11RaP',
+        'year': '2011'
+        'make': 'Ford',
+        'model': 'F150',
+        'trim': 'SVT Raptor',
+        'color': 'Ingot Silver',
+        'img': 'https://www.cstatic-images.com/car-pictures/xl/CAC10FOT11EA0101.png'
+    },
+
+    {
+        'id': '123456',
+        'make': 'Aucura',
+        'model': 'MDX',
+        'year': '2003',
+        'color': 'silver',
+        'img': 'acura_mdx_2003.jpg',
+    },
+
+
+];
